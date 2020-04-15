852030ab7485e5e7cc31149b772f8eeb4fd982b2
Mojo <1316578052@qq.com>
2020-04-16 04:19:45 +0800

    2

---
+++ springboot-dubbo-api/src/main/java/cn/linyt/study/api/App.java
@@ -0,0 +1,15 @@
+package cn.linyt.study.api;
+
+/**
+ * @ClassName App
+ * @Description TODO
+ * @Author Mojo
+ * @Date 2020/4/16 4:19
+ * @Version 1.0
+ **/
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
