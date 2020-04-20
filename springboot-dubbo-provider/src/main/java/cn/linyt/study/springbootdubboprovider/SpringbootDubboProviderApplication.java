49698e48481957998b2710c81c0676d82fe4de91
Mojo <1316578052@qq.com>
2020-04-20 08:56:54 +0800

    7

--- springboot-dubbo-provider/src/main/java/cn/linyt/study/springbootdubboprovider/SpringbootDubboProviderApplication.java
+++ springboot-dubbo-provider/src/main/java/cn/linyt/study/springbootdubboprovider/SpringbootDubboProviderApplication.java
@@ -1,10 +1,8 @@
 package cn.linyt.study.springbootdubboprovider;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableDubbo(scanBasePackages = "cn.linyt.study.springbootdubboprovider.service.impl")
 @SpringBootApplication
 public class SpringbootDubboProviderApplication {
 
