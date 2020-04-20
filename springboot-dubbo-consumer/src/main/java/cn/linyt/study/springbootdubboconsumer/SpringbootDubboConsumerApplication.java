65e3094f961fa37230a073086b853cb581838166
Mojo <1316578052@qq.com>
2020-04-20 08:50:46 +0800

    6

--- springboot-dubbo-consumer/src/main/java/cn/linyt/study/springbootdubboconsumer/SpringbootDubboConsumerApplication.java
+++ springboot-dubbo-consumer/src/main/java/cn/linyt/study/springbootdubboconsumer/SpringbootDubboConsumerApplication.java
@@ -4,7 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableDubbo(scanBasePackages = "cn.linyt.study.springbootdubboconsumer.controller")
+//@EnableDubbo(scanBasePackages = "cn.linyt.study.springbootdubboconsumer.controller")
 @SpringBootApplication
 public class SpringbootDubboConsumerApplication {
 
--- springboot-dubbo-provider/src/main/java/cn/linyt/study/springbootdubboprovider/SpringbootDubboProviderApplication.java
+++ springboot-dubbo-provider/src/main/java/cn/linyt/study/springbootdubboprovider/SpringbootDubboProviderApplication.java
@@ -4,7 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableDubbo(scanBasePackages = "cn.linyt.study.springbootdubboprovider.service.impl")
+//@EnableDubbo(scanBasePackages = "cn.linyt.study.springbootdubboprovider.service.impl")
 @SpringBootApplication
 public class SpringbootDubboProviderApplication {
 
